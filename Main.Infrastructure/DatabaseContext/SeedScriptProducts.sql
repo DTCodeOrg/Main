74cfda9b8b53c160413d899a03ed12c585df5f0a
Naim <naimul.prodhan81@hotmail.com>
2026-09-06 07:10:41 +0600

    Seed Pages table with explicit PageID for tenant 3
    Added multiple INSERTs to [dbo].[Pages] in CloneTenantDatabase for tenant '00000003-0000-0000-0000-000000000000', enabling IDENTITY_INSERT to set PageID explicitly. Each page is active with TenantCountry = 1. Changes follow Tenants table seeding.

--- Main.Infrastructure/DatabaseContext/SeedScriptProducts.sql
+++ Main.Infrastructure/DatabaseContext/SeedScriptProducts.sql
@@ -21,3 +21,24 @@ INSERT [dbo].[Tenants] ([TenantId], [TenantName], [HostType], [Host], [SecretKey
 VALUES (N'00000004-0000-0000-0000-000000000000', N'lifestyles', 0, N'lifestyles', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL)
 
 GO
+
+USE [CloneTenantDatabase]
+GO
+SET IDENTITY_INSERT [dbo].[Pages] ON 
+GO
+INSERT [dbo].[Pages] ([PageID], [EnumPublicPage], [MyTenantId], [CreatedBy], [ModifiedBy], [DeletedBy], [CreatedDate], [ModifiedDate], [DeletedDate], [IsActive], [TenantCountry], [TenantContinent]) VALUES (19, 1, N'00000003-0000-0000-0000-000000000000', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, NULL)
+GO
+INSERT [dbo].[Pages] ([PageID], [EnumPublicPage], [MyTenantId], [CreatedBy], [ModifiedBy], [DeletedBy], [CreatedDate], [ModifiedDate], [DeletedDate], [IsActive], [TenantCountry], [TenantContinent]) VALUES (20, 3, N'00000003-0000-0000-0000-000000000000', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, NULL)
+GO
+INSERT [dbo].[Pages] ([PageID], [EnumPublicPage], [MyTenantId], [CreatedBy], [ModifiedBy], [DeletedBy], [CreatedDate], [ModifiedDate], [DeletedDate], [IsActive], [TenantCountry], [TenantContinent]) VALUES (21, 8, N'00000003-0000-0000-0000-000000000000', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, NULL)
+GO
+INSERT [dbo].[Pages] ([PageID], [EnumPublicPage], [MyTenantId], [CreatedBy], [ModifiedBy], [DeletedBy], [CreatedDate], [ModifiedDate], [DeletedDate], [IsActive], [TenantCountry], [TenantContinent]) VALUES (22, 4, N'00000003-0000-0000-0000-000000000000', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, NULL)
+GO
+INSERT [dbo].[Pages] ([PageID], [EnumPublicPage], [MyTenantId], [CreatedBy], [ModifiedBy], [DeletedBy], [CreatedDate], [ModifiedDate], [DeletedDate], [IsActive], [TenantCountry], [TenantContinent])VALUES (23, 5, N'00000003-0000-0000-0000-000000000000', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, NULL)
+GO
+INSERT [dbo].[Pages] ([PageID], [EnumPublicPage], [MyTenantId], [CreatedBy], [ModifiedBy], [DeletedBy], [CreatedDate], [ModifiedDate], [DeletedDate], [IsActive], [TenantCountry], [TenantContinent]) VALUES (24, 6, N'00000003-0000-0000-0000-000000000000', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, NULL)
+GO
+INSERT [dbo].[Pages] ([PageID], [EnumPublicPage], [MyTenantId], [CreatedBy], [ModifiedBy], [DeletedBy], [CreatedDate], [ModifiedDate], [DeletedDate], [IsActive], [TenantCountry], [TenantContinent]) VALUES (25, 2, N'00000003-0000-0000-0000-000000000000', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, NULL)
+GO
+INSERT [dbo].[Pages] ([PageID], [EnumPublicPage], [MyTenantId], [CreatedBy], [ModifiedBy], [DeletedBy], [CreatedDate], [ModifiedDate], [DeletedDate], [IsActive], [TenantCountry], [TenantContinent]) VALUES (26, 7, N'00000003-0000-0000-0000-000000000000', NULL, NULL, NULL, NULL, NULL, NULL, 1, 1, NULL)
+GO
